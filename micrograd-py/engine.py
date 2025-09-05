0a9b81f63a2243ed2132c4bf2fec689dafb07cd9
saagark <saagarsk@gmail.com>
2025-09-04 21:37:00 -0700

    Add backwards function, need to add more operations to Value

--- micrograd-py/engine.py
+++ micrograd-py/engine.py
@@ -23,5 +23,19 @@ class Value:
         out._backward = _backward
         return out
 
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._children:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1
+        for node in reversed(topo):
+            node._backward()
+
     def __repr__(self):
         return f"Value(data={self.data}, self.grad={self.grad})"
