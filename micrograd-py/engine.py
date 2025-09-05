02985637f1e11486dc2df79e54d5535ee2dbba31
saagark <saagarsk@gmail.com>
2025-09-04 21:47:38 -0700

    add more functions, test

--- micrograd-py/engine.py
+++ micrograd-py/engine.py
@@ -1,10 +1,11 @@
 
 class Value:
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
         self._children = set(_children)
         self._op = _op
+        self.label = label
         self._backward = lambda: None # autograd function
 
     def __add__(self, other):
@@ -23,6 +24,27 @@ class Value:
         out._backward = _backward
         return out
 
+    def __pow__(self, other):
+        out = Value(self.data**other, (self,), f'**{other}')
+        def _backward():
+            self.grad += other * self.data**(other-1) * out.grad
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = Value(max(0, self.data), (self,), 'relu')
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+        return out
+
+    def sigmoid(self):
+        out = Value(1 / (1 + math.exp(-self.data)), (self,), 'sigmoid')
+        def _backward():
+            self.grad += out.data * (1 - out.data) * out.grad
+        out._backward = _backward
+        return out
+
     def backward(self):
         topo = []
         visited = set()
@@ -36,6 +58,27 @@ class Value:
         self.grad = 1
         for node in reversed(topo):
             node._backward()
+
+    def __neg__(self):
+        return self * -1
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __rtruediv__(self, other):
+        return other * self**-1
 
     def __repr__(self):
         return f"Value(data={self.data}, self.grad={self.grad})"
